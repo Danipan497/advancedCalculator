acaddf33b8657a05b3f4734441d3ecaaeae6b5bd
Danipan497 <danipan497@gmail.com>
2022-08-18 19:56:47 +0200

    First row of elements.

--- advancedCalculator/Form1.Designer.cs
+++ advancedCalculator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace advancedCalculator
 {
-    partial class Form1
+    partial class Calculator
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.zeroButton = new System.Windows.Forms.Button();
+            this.plusMinusButton = new System.Windows.Forms.Button();
+            this.decimalButton = new System.Windows.Forms.Button();
+            this.equalButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zeroButton
+            // 
+            this.zeroButton.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.zeroButton.Location = new System.Drawing.Point(176, 652);
+            this.zeroButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.zeroButton.Name = "zeroButton";
+            this.zeroButton.Padding = new System.Windows.Forms.Padding(1);
+            this.zeroButton.Size = new System.Drawing.Size(158, 114);
+            this.zeroButton.TabIndex = 0;
+            this.zeroButton.Text = "0";
+            this.zeroButton.UseVisualStyleBackColor = true;
+            this.zeroButton.Click += new System.EventHandler(this.zeroButton_Click);
+            // 
+            // plusMinusButton
+            // 
+            this.plusMinusButton.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.plusMinusButton.Location = new System.Drawing.Point(12, 652);
+            this.plusMinusButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.plusMinusButton.Name = "plusMinusButton";
+            this.plusMinusButton.Padding = new System.Windows.Forms.Padding(1);
+            this.plusMinusButton.Size = new System.Drawing.Size(158, 114);
+            this.plusMinusButton.TabIndex = 1;
+            this.plusMinusButton.Text = "+/-";
+            this.plusMinusButton.UseVisualStyleBackColor = true;
+            // 
+            // decimalButton
+            // 
+            this.decimalButton.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.decimalButton.Location = new System.Drawing.Point(340, 652);
+            this.decimalButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.decimalButton.Name = "decimalButton";
+            this.decimalButton.Padding = new System.Windows.Forms.Padding(1);
+            this.decimalButton.Size = new System.Drawing.Size(158, 114);
+            this.decimalButton.TabIndex = 2;
+            this.decimalButton.Text = ".";
+            this.decimalButton.UseVisualStyleBackColor = true;
+            // 
+            // equalButton
+            // 
+            this.equalButton.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.equalButton.Location = new System.Drawing.Point(504, 652);
+            this.equalButton.MinimumSize = new System.Drawing.Size(158, 114);
+            this.equalButton.Name = "equalButton";
+            this.equalButton.Padding = new System.Windows.Forms.Padding(1);
+            this.equalButton.Size = new System.Drawing.Size(158, 114);
+            this.equalButton.TabIndex = 3;
+            this.equalButton.Text = "=";
+            this.equalButton.UseVisualStyleBackColor = true;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(670, 778);
+            this.Controls.Add(this.equalButton);
+            this.Controls.Add(this.decimalButton);
+            this.Controls.Add(this.plusMinusButton);
+            this.Controls.Add(this.zeroButton);
+            this.MinimumSize = new System.Drawing.Size(692, 834);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Calculator_Load);
+            this.Resize += new System.EventHandler(this.Calculator_Resize);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button zeroButton;
+        private Button plusMinusButton;
+        private Button decimalButton;
+        private Button equalButton;
     }
 }
--- advancedCalculator/Form1.cs
+++ advancedCalculator/Form1.cs
@@ -1,10 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace advancedCalculator
 {
-    public partial class Form1 : Form
+    public partial class Calculator : Form
     {
-        public Form1()
+        private Rectangle zeroButtonOriginalRectangle;
+        private Rectangle plusMinusButtonOriginalRectangle;
+        private Rectangle decimalButtonOriginalRectangle;
+        private Rectangle equalButtonOriginalRectangle;
+        private Size originalFormSize;
+        public Calculator()
         {
             InitializeComponent();
+        }
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+            originalFormSize = this.Size;
+            zeroButtonOriginalRectangle = new Rectangle(zeroButton.Location.X, zeroButton.Location.Y, zeroButton.Width, zeroButton.Height);
+            plusMinusButtonOriginalRectangle = new Rectangle(plusMinusButton.Location.X, plusMinusButton.Location.Y, plusMinusButton.Width, plusMinusButton.Height);
+            decimalButtonOriginalRectangle = new Rectangle(decimalButton.Location.X, decimalButton.Location.Y, decimalButton.Width, decimalButton.Height);
+            equalButtonOriginalRectangle = new Rectangle(equalButton.Location.X, equalButton.Location.Y, equalButton.Width, equalButton.Height);
+        }
+
+        private void Calculator_Resize(object sender, EventArgs e)
+        {
+            resizeControl(zeroButtonOriginalRectangle, zeroButton);
+            resizeControl(plusMinusButtonOriginalRectangle, plusMinusButton);
+            resizeControl(decimalButtonOriginalRectangle, decimalButton);
+            resizeControl(equalButtonOriginalRectangle, equalButton);
+        }
+                private void resizeControl(Rectangle OriginalControlRect, Control control)
+        {
+            float xAxis = (float)(this.Width) / (float)(originalFormSize.Width);
+            float yAxis = (float)(this.Height) / (float)(originalFormSize.Height);
+
+            int newXPosition = (int)(OriginalControlRect.X * xAxis);
+            int newYPosition = (int)(OriginalControlRect.Y * yAxis);
+
+            int newWidth = (int)(OriginalControlRect.Width * xAxis);
+            int newHeight = (int)(OriginalControlRect.Height * yAxis);
+
+            control.Location = new Point(newXPosition, newYPosition);
+            control.Size = new Size(newWidth, newHeight);
+        }
+
+        private void zeroButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
